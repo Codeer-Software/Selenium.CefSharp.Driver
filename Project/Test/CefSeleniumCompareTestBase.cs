e8fdefc44b039dfd746e54c946450fb4f536aded
Kazuhide Maruyama <afurock@gmail.com>
2019-12-28 15:13:45 +0900

    rename test method names to english.

--- Project/Test/CefSeleniumCompareTestBase.cs
+++ Project/Test/CefSeleniumCompareTestBase.cs
@@ -1,62 +1,128 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Codeer.Friendly.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using Selenium.CefSharp.Driver;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+
+using Codeer.Friendly.Dynamic;
+using OpenQA.Selenium.Chrome;
 
 namespace Test
 {
-    public abstract class SpikesBase
+    [TestClass]
+    public class CefSelemiumCompareTestForCef : CefSeleniumCompareTestBase
+    {
+        WindowsAppFriend _app;
+        CefSharpDriver _driver;
+        string _htmlPath;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //start process.
+            var dir = GetType().Assembly.Location;
+            for (int i = 0; i < 4; i++) dir = Path.GetDirectoryName(dir);
+            var processPath = Path.Combine(dir, @"CefSharpWPFSample\bin\x86\Debug\CefSharpWPFSample.exe");
+            var process = Process.Start(processPath);
+
+            //html
+            _htmlPath = Path.Combine(dir, @"Test\Controls.html");
+
+            //attach by friendly.
+            _app = new WindowsAppFriend(process);
+            var main = _app.Type<Application>().Current.MainWindow;
+
+            //create driver.
+            _driver = new CefSharpDriver(main._browser);
+            _driver.Url = _htmlPath;
+        }
+
+        [TestCleanup]
+        public void TestCleanup() => Process.GetProcessById(_app.ProcessId).Kill();
+
+        public override IWebDriver GetDriver() => _driver;
+    }
+
+    [TestClass]
+    public class CefSelemiumCompareTestForSelenium : CefSeleniumCompareTestBase
+    {
+        IWebDriver _driver;
+
+        public override IWebDriver GetDriver() => _driver;
+
+        [TestInitialize]
+        public void initialize()
+        {
+            _driver = new ChromeDriver();
+            var dir = GetType().Assembly.Location;
+            for (int i = 0; i < 4; i++) dir = Path.GetDirectoryName(dir);
+
+            _driver.Url = Path.Combine(dir, @"Test\Controls.html");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _driver.Dispose();
+        }
+    }
+
+    public abstract class CefSeleniumCompareTestBase
     {
         public abstract IWebDriver GetDriver();
         
         [TestMethod]
-        public void 単純なシングルクォーテーションで囲まれた文字列()
+        public void ShouldReturnSameStringWhenExecuteReturnSingleQuotationStringJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return 'abcde'");
             Assert.AreEqual("abcde", value);
         }
 
         [TestMethod]
-        public void 単純なダブルクォーテーションで囲まれた文字列()
+        public void ShouldReturnSameStringWhenExecuteReturnDoubleQuotationStringJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return \"abcde\"");
             Assert.AreEqual("abcde", value);
         }
 
         [TestMethod]
-        public void 単純なバッククォーテーションで囲まれた文字列()
+        public void ShouldReturnSameStringWhenExecuteReturnBackQuotationStringJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return `abcde`");
             Assert.AreEqual("abcde", value);
         }
 
         [TestMethod]
-        public void 単純な整数値_long()
+        public void ShouldReturnLongValueWhenExecuteReturnIntegerJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return 12345");
             Assert.AreEqual(12345L, value);
         }
 
         [TestMethod]
-        public void 単純な小数点()
+        public void ShouldReturnDoubleValueWhenExecuteReturnDecimalJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return 12345.678");
             Assert.AreEqual(12345.678, value);
         }
 
         [TestMethod]
-        public void 単純な真偽値()
+        public void ShouldReturnBooleanValueWhenExecuteReturnBooleanJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return false");
             Assert.IsFalse((bool)value);
         }
 
         [TestMethod]
-        public void 単純な日付()
+        public void ShouldReturnIsoDateStringWhenExecuteReturnDateJavaScript()
         {
             // selenium (のChrome Driver) は ISOString の結果を返す模様
             var isoValue = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return new Date(2012,0,16,23,23,23,123).toISOString()");
@@ -65,7 +131,7 @@ namespace Test
         }
 
         [TestMethod]
-        public void 無効な日付()
+        public void ShouldReturnNullValueWhenExecuteReturnInvalidJavaScript()
         {
             // selenium (のChrome Driver) は Invalid Date は null を返す模様
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return new Date(void 0)");
@@ -73,35 +139,35 @@ namespace Test
         }
 
         [TestMethod]
-        public void アンデファインド()
+        public void ShouldReturnNullValueWhenExecuteReturnUndefinedJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return void 0");
             Assert.IsNull(value);
         }
 
         [TestMethod]
-        public void ポジティブインフィニティ()
+        public void ShouldReturnNullValueWhenExecuteReturnPositiveInfinitJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return Number.POSITIVE_INFINITY");
             Assert.IsNull(value);
         }
 
         [TestMethod]
-        public void ネガティブインフィニティ()
+        public void ShouldReturnNullValueWhenExecuteReturnNegativeInfinitJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return Number.NEGATIVE_INFINITY");
             Assert.IsNull(value);
         }
 
         [TestMethod]
-        public void ナン()
+        public void ShouldReturnNullValueWhenExecuteReturnNaNJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return Number.NaN");
             Assert.IsNull(value);
         }
 
         [TestMethod]
-        public void 単純な配列()
+        public void ShouldReturnReadOnlyCollectionObjectValueWhenExecuteReturnArrayJavaScript()
         {
             // selenium (のChrome Driver) は ReadOnlyCollection を返す模様
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return [123, 456, 789]");
@@ -118,7 +184,7 @@ namespace Test
         }
 
         [TestMethod]
-        public void JavaScript側で型変換が入る配列()
+        public void ShouldReturnTypeMoldeConvertedValueOfElementsInReadOnlyCollectionObjectValueWhenExecuteReturnArrayJavaScript()
         {
             var isoValue = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return new Date(2012,0,16,23,23,23,123).toISOString()");
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return [Number.NaN, Number.POSITIVE_INFINITY, new Date(void 0), new Date(2012,0,16,23,23,23,123), 123]");
@@ -135,7 +201,7 @@ namespace Test
         }
 
         [TestMethod]
-        public void 単純なオブジェクト()
+        public void ShouldReturnDictionalyStringObjectWhenExecuteReturnObjectJavaScript()
         {
             var value = ((IJavaScriptExecutor)GetDriver()).ExecuteScript("return {A:'a', B:123, C: false}");
             Assert.AreEqual(typeof(Dictionary<string, object>), value.GetType());
