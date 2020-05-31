c34fe0fcf1c912e3c149143e85265a789f3b7f0a
Kazuhide Maruyama <afurock@gmail.com>
2020-05-31 11:35:57 +0900

    add first SelectElement test.

---
+++ Project/Test/SelectElementTest.cs
@@ -0,0 +1,123 @@
+﻿using Codeer.Friendly.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Selenium.CefSharp.Driver;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass]
+    public class WinFormSelectElementTest : SelectElementTestBase
+    {
+        static WindowsAppFriend _app;
+        static CefSharpDriver _driver;
+
+        public override IWebDriver GetDriver() => _driver;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _driver.Url = this.GetHtmlUrl();
+        }
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            ClassInitBase();
+
+            var appWithDriver = AppRunner.RunWinFormApp();
+            _app = appWithDriver.App;
+            _driver = appWithDriver.Driver;
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Process.GetProcessById(_app.ProcessId).Kill();
+            ClassCleanupBase();
+        }
+    }
+
+    [TestClass]
+    public class WpfSelectElementTest : SelectElementTestBase
+    {
+        static WindowsAppFriend _app;
+        static CefSharpDriver _driver;
+
+        public override IWebDriver GetDriver() => _driver;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _driver.Url = this.GetHtmlUrl();
+        }
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            ClassInitBase();
+            var appWithDriver = AppRunner.RunWpfApp();
+            _app = appWithDriver.App;
+            _driver = appWithDriver.Driver;
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Process.GetProcessById(_app.ProcessId).Kill();
+            ClassCleanupBase();
+        }
+    }
+
+    [TestClass]
+    public class SeleniumSelectElementTest : SelectElementTestBase
+    {
+        static IWebDriver _driver;
+
+        public override IWebDriver GetDriver() => _driver;
+
+        [TestInitialize]
+        public void initialize()
+        {
+            _driver.Url = this.GetHtmlUrl();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            ClassInitBase();
+            _driver = new ChromeDriver();
+        }
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            _driver.Dispose();
+            ClassCleanupBase();
+        }
+    }
+
+    public abstract class SelectElementTestBase : CompareTestBase
+    {
+        [TestMethod]
+        public void SelectByText()
+        {
+            var select = GetDriver().FindElement(By.Id("singleSelect"));
+            var elem = new SelectElement(select);
+            elem.SelectByText("ABCDE");
+            var selectedValue = select.GetProperty("value");
+            selectedValue.Is("1");
+        }
+    }
+}
